97b779e5c1d280632dfac73d4f44baae1e067632
lucasthormann <LucasThormann@protonmail.com>
2024-08-23 14:34:24 -0500

    Conversion to & Generation of assembly file

--- src/main.cpp
+++ src/main.cpp
@@ -73,6 +73,29 @@ std::vector<Token> tokenize(const std::string& str)
   return tokens;
 }
 
+std::string tokens_to_asm(const std::vector<Token>& tokens)
+{
+  std::stringstream output;
+  output << "global _start\n_start:\n";
+  for(int i = 0; i < tokens.size(); i++)
+  {
+    const Token& token = tokens.at(i);
+    if(token.type == TokenType::_return)
+    {
+      if(i + 1 < tokens.size() && tokens.at(i + 1).type == TokenType::int_lit)
+      {
+        if(i + 2 < tokens.size() && tokens.at(i + 2).type == TokenType::semi)
+        {
+          output << "    mov rax, 60\n";
+          output << "    mov rdi, " << tokens.at(i + 1).value.value() << "\n";
+          output << "    syscall";
+        }
+      }
+    }
+  }
+  return output.str();
+}
+
 int main(int argc, char* argv[]){
   if(argc != 2)
   {
@@ -89,8 +112,11 @@ int main(int argc, char* argv[]){
     contents = contents_stream.str();
   }
 
-  std::vector<Token> thing = tokenize(contents);
-
+  std::vector<Token> tokens = tokenize(contents);
+  {
+    std::fstream file("../out.asm", std::ios::out);
+    file << tokens_to_asm(tokens);
+  }
 
 
   return EXIT_SUCCESS;
